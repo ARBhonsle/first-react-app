08eff7594d056ab98f224a9c342813abcb93e337
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-18 18:05:40 +0530

    Modified: Display Hello Message

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
+import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to First React App!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello from BridgeLabz
-        </a>
-        <a
-          className="App-link"
-          href="https://nodejs.org/en/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn NodeJS
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Bridgelabz</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
