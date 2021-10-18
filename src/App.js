8c46863dbc9fd0481b3e658dd04b09fbff71caad
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-18 17:48:44 +0530

    Added: Project files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to First React App!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello from BridgeLabz
+        </a>
+        <a
+          className="App-link"
+          href="https://nodejs.org/en/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn NodeJS
         </a>
       </header>
     </div>
